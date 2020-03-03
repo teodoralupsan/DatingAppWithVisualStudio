efa09d9bdae6a0883a6c4fad4bbd6495c1f85fe9
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-03-03 15:29:25 +0200

    Section4: Client side login and register

---
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseUrl = 'http://localhost:60671/api/auth';
+
+constructor(private http: HttpClient) { }
+
+login(model: any){
+  return this.http.post(`${this.baseUrl}/login`, model).pipe(
+    map((response: any) => {
+      if (response) {
+        localStorage.setItem('token', response.token);
+      }
+    })
+  );
+}
+
+}
