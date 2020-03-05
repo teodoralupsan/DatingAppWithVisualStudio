3b83f7ec77f3f0c4af31bc211b62877d43e9295d
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-03-05 16:19:30 +0200

    59. Adding Ngx Bootstrap

--- DatingApp-SPA/src/app/app.component.ts
+++ DatingApp-SPA/src/app/app.component.ts
@@ -8,8 +8,6 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  title = 'DatingApp-SPA with VisualStudio';
-
   jwtHelper = new JwtHelperService();
 
   constructor(private authService: AuthService) {}
