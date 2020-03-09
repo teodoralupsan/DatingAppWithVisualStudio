6465c66a8360890ba8a7b7bd3f8768b5288ed0c8
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-03-09 12:19:09 +0200

    85.Creating member cards

--- DatingApp.API/Dtos/UserForListDto.cs
+++ DatingApp.API/Dtos/UserForListDto.cs
@@ -16,6 +16,6 @@ namespace DatingApp.API.Dtos
         public DateTime LastActive { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
-        public string PhotopUrl { get; set; }
+        public string PhotoUrl { get; set; }
     }
 }
--- DatingApp.API/Helpers/AutoMapperProfiles.cs
+++ DatingApp.API/Helpers/AutoMapperProfiles.cs
@@ -13,7 +13,7 @@ namespace DatingApp.API.Helpers
         public AutoMapperProfiles()
         {
             CreateMap<User, UserForListDto>()
-                .ForMember(dest => dest.PhotopUrl, opt => 
+                .ForMember(dest => dest.PhotoUrl, opt => 
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                 .ForMember(dest => dest.Age, opt =>
                     opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
