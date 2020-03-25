26515f9c905e24d36c1b07c01f1fe17a9a7fd844
Teodora Lupsan <teodora_pintea_ramona@yahoo.com>
2020-03-25 17:15:33 +0200

    143,144.Using ngx-bootstrap pagination module

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'src/app/_services/user.service';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { Pagination, PaginatedResult } from 'src/app/_models/pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  pagination: Pagination;
 
   constructor(
     private userService: UserService,
@@ -19,17 +21,24 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
     });
   }
 
-  /*
+  pageChanged(event: any) {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
   loadUsers() {
-    this.userService.getUsers().subscribe((userList: User[]) => {
-      this.users = userList;
+    this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<User[]>) => {
+        this.users = res.result;
+        this.pagination = res.pagination;
     }, error => {
       this.alertify.error(error);
     });
-  }*/
+  }
 
 }
